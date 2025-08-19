fa32a9f88efdeaad9c6995f92872edc44f3c33f4
Nerilka <jamansik@hotmail.com>
2025-08-19 12:54:55 +0300

    Laskin tehty

--- Laskin/App.js
+++ Laskin/App.js
@@ -1,12 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [eka, setEka] = useState("");
+  const [toka, setToka] = useState("");
+  const [result, setResult] = useState(null);
+
+  const buttonPlus = () => {
+    const sum = parseFloat(eka) + parseFloat(toka);
+    setResult(sum);
+  };
+
+  const buttonMinus = () => {
+    const minus = parseFloat(eka) - parseFloat(toka);
+    setResult(minus);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: {result}</Text>
+      <View>
+        <TextInput
+          style={styles.nums}
+          placeholder='Give number'
+          keyboardType="numeric"
+          onChangeText={eka => setEka(eka)}
+          value={eka}
+        />
+        <TextInput
+          style={styles.nums}
+          placeholder='Give number'
+          keyboardType="numeric"
+          onChangeText={toka => setToka(toka)}
+          value={toka}
+        />
+      </View>
+      <View style={styles.button}>
+        <View style={styles.onebutton}>
+          <Button onPress={buttonPlus} title="+" />
+        </View>
+        <View style={styles.onebutton}>
+          <Button onPress={buttonMinus} title="-" />
+        </View>
+      </View>
     </View>
+
+
   );
 }
 
@@ -16,5 +57,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
+  nums: {
+    borderWidth: 1,
+    width: 200,
+  },
+  button: {
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  onebutton: {
+    marginHorizontal: 10,
+    width: 35,
+  }
 });
