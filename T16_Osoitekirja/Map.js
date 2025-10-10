56e8cacf7a23e8cb1950dceff118ee4efef6c0de
Janette Luomala <jamansik@hotmail.com>
2025-10-10 15:13:52 +0300

    Kartta otettu käyttöön

--- T16_Osoitekirja/Map.js
+++ T16_Osoitekirja/Map.js
@@ -1,12 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useState } from 'react';
 
 export default function Map({ route }) {
 
     const { infoToPass } = route.params;
-    
+    const [region, setRegion] = useState({
+        latitude: 60.200692,
+        longitude: 24.934302,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+    })
+
     return (
         <View style={styles.container}>
             <Text>Welcome to our App! </Text>
+
+            <MapView
+                style={{ width: '100%', height: '80%' }}
+                region={region}
+            >
+            <Marker
+                coordinate={region}
+                title='Haaga-Helia'
+            />
+            </MapView>
         </View>
     );
 }
